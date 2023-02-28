4430053230ed8ecaf23b7cce32be6262449c2389
Chanhvd <125170214+Chanhvd@users.noreply.github.com>
2023-02-28 15:49:52 +0700

    Update downhinh_disco.py

--- downhinh_disco.py
+++ downhinh_disco.py
@@ -1,47 +1,5 @@
 import random
 import re
-from tkinter import  filedialog
-from selenium import webdriver
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import nfc
-import time
-from PIL import Image
-import os
-import cv2
-import requests
-from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
-# Khởi tạo trình duyệt Chromium
-# user_agent = UserAgent()
-# random_user_agent = user_agent.random
-# # print(random_user_agent)
-# browser = webdriver.Chrome()
-# chrome_options = Options()
-# chrome_options.add_argument("--incognito")
-# chrome_options.add_argument("--disable-extensions")
-# chrome_options.add_argument("--start-maximized")
-# chrome_options.add_argument("--disable-web-security")
-# chrome_options.add_argument("--no-proxy-server")
-# chrome_options.add_argument("--disable-gpu")
-# chrome_options.add_argument("--disable-notifications")
-# chrome_options.add_argument("--disable-popup-blocking")
-# chrome_options.add_argument("--disable-infobars")
-# # chrome_options.add_argument(random_user_agent)
-# chrome_options.add_argument('--disable-features=VizDisplayCompositor')
-# # chrome_options.add_argument("--headless") # ẩn trình duyệt
-
-# # browser.execute_script("Object.defineProperty(navigator, 'getDisplayMedia', {get: function() {return {};},});")
-# browser.execute_script("Object.defineProperty(navigator, 'bluetooth', {get: function() {return {requestDevice: function() {return {};}};},});")
-# browser.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5];},});")
-# browser.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['en-US', 'en'];}})")
-# browser = webdriver.Chrome(executable_path='chromedriver.exe', options=chrome_options)
-    # Cài đặt các thiết lập cơ bản
-# browser.set_window_size(500, 800)
-# browser.set_window_position(200, 0)
-    # Mở trang web
-import re
 import os
 import discord
 import requests
@@ -52,8 +10,8 @@ url = 'https://discord.com/api/v9/auth/login'
 headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 payload = {
-        'login': 'kt.dptech@gmail.com',
-        'password': 'Chanh983845',
+        'login': 'email',
+        'password': 'pass',
         'undelete': False,
         'captcha_key': None,
         'gift_code_sku_id': None,
@@ -70,7 +28,7 @@ if match:
 while True:
     
     headers = {
-        'Authorization': 'MTA3OTk2Mjk3NTg4NjExODk4Mw.GFVphU.uVJo3ecunaq8jjCU8eF58WTftfDIUbEk5NFjjs'
+        'Authorization': token
     }
 
     channel_id = '997268018855940147'
@@ -149,44 +107,3 @@ while True:
         print('Done.')    
     directory = 'images1/'
     delete_duplicate_images(directory)
-    # Break the loop after 1 minute
- # def compare_images(image1_path, image2_path):
-    #     image1 = Image.open(image1_path)
-    #     image2 = Image.open(image2_path)
-    #     if image1.mode != image2.mode or image1.size != image2.size:
-    #         return False
-    #     return ImageChops.difference(image1, image2).getbbox() is None
-
-    # def delete_duplicate_images(directory):
-    #     files = os.listdir(directory)
-    #     print(files)
-    #     for i in range(len(files)):
-    #         image1_path = os.path.join(directory, files[i])
-    #         print('i',i)
-    #         for j in range(i+1, len(files)):
-    #             image2_path = os.path.join(directory, files[j])
-    #             print('J',j)
-    #             if compare_images(image1_path, image2_path):
-    #                 os.remove(image1_path)
-    #                 print('a')
-    #                 break
-
-    # directory = 'images1/'
-    # print('........')
-    # delete_duplicate_images(directory)
-       
-    
-
-# # Lặp qua từng đường dẫn để tải hình ảnh
-# for idx, link in enumerate(image_links):
-#     response = requests.get(link)
-#     # Kiểm tra xem phản hồi có thành công không
-#     if response.status_code == 200:
-#         # Tạo tên tệp tin mới bằng cách thêm chỉ số vào đầu tên
-#         filename = f'image_{idx}.jpg'
-#         # Ghi nội dung hình ảnh vào tệp tin
-#         with open(f'images1/{filename}', 'wb') as f:
-#             f.write(response.content)
-#         print(f'{filename} saved.')
-#     else:
-#         print(f'Error downloading image from {link}')
